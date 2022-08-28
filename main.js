22ca767a0a57dcb6aef0a2a4918eea93c1087ab2
Jéssica da Conceiçao <daconceicaoj792@gmail.com>
2022-08-28 15:56:50 -0300

    teste

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 window.alert("Olá,☺🖥 Seja bem vindo ao site.")
 
-(function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://https-j4essika-github-io-the-end.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
+
                   
