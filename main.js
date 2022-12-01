f78a0b56c72d22d87e3876535e49d6c091226086
Jéssica da Conceiçao <daconceicaoj792@gmail.com>
2022-12-01 09:30:30 -0300

    alteraçao

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 window.alert('Olá,seja bem vindo(a) ao site.')
+let nome = ' Jessica Souza'
+console.log('Jéssica Souza 2022')
 
 let h1 = window.document.getElementsByTagName('h1')[0]
 h1.style.color='darkred'
